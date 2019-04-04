9020743a120a008ca5b13e9db85800774b8811e2
mominirfan <momin.irfan@live.com>
2019-04-04 00:03:35 -0500

    adding CNAME and other stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public mobile;
   title = 'smu-one';
-  names = ['Canvas','My.SMU','Connect.SMU','Is Macs Open', 'Meme Page', 'SMU E-Mail','Daily Campus'];
-  links = ['https://smu.instructure.com/','https://my.smu.edu/','https://smu.campuslabs.com/engage/','https://s2.smu.edu/~mirfan/ismacsopen.html', 'https://www.facebook.com/groups/2252432648315371/','http://outlook.com/owa/mail.physics.smu.edu/','https://www.smudailycampus.com/'];
+  names = ['Canvas','My.SMU','Connect.SMU','Is Macs Open','Is Post-Office Open', 'Meme Page', 'SMU E-Mail','Daily Campus'];
+  links = ['https://smu.instructure.com/','https://my.smu.edu/','https://smu.campuslabs.com/engage/','https://s2.smu.edu/~mirfan/ismacsopen.html','https://s2.smu.edu/~mirfan/ispostopen.html', 'https://www.facebook.com/groups/2252432648315371/','http://outlook.com/owa/mail.physics.smu.edu/','https://www.smudailycampus.com/'];
 
   ngOnInit() {
-    if (window.screen.width <= 400) { // 768px portrait
+    if (window.screen.width <= 700) { // 768px portrait
       this.mobile = true;
     }
   }
